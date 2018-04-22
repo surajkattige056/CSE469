82e776c5ffda3a0f3b26074927fd0f36896b67e3
aalagili <35924124+aalagili@users.noreply.github.com>
2018-04-22 06:32:37 -0700

    updated version to match address4forensics.cc

--- address4forensics.h
+++ address4forensics.h
@@ -1,17 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <iostream>
-#include <tuple>
 
-struct output_pairs{
-	int number = 0;
+
+using namespace std;
+
+struct output_pairs {
+	long long number = 0;
 	bool flag = false;
-	bool error = false;	
-};
-
-class address4forensics{
-	public:
-		output_pairs argument_analyzer(std::string a, std::string b);
-		std::tuple<std::string, std::string, int> number_retriever(std::string s);   
-        
+	bool error = false;
 };
